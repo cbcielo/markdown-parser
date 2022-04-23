a81fee50aae4afc7858deea1d5e77b597059152d
cbcielo <ccielo@ucsd.edu>
2022-04-23 15:31:40 -0700

    fix infinite loop
    for test file 2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,21 +11,29 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+
         while(currentIndex < markdown.length()) {
+
+            System.out.println(currentIndex);
+
             int openBracket = markdown.indexOf("[", currentIndex);
+
+            if (openBracket == -1) {
+                break;
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", currentIndex);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
 
-            System.out.println(currentIndex);
+            
         }
 
         return toReturn;
     }
 
-/*
+
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
@@ -33,5 +41,5 @@ public class MarkdownParse {
 	    System.out.println(links);
         System.out.println("fin.");
     }
-*/
+
 }
