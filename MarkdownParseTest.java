6d32871bcd6c87bb490eaa0e89d68edd96cced0b
cbcielo <ccielo@ucsd.edu>
2022-04-28 08:24:02 -0700

    adding test file

---
+++ MarkdownParseTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.*;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinks() throws IOException {
+        Path filePath = Path.of("test-file.md");
+        String content = Files.readString(filePath);
+
+        ArrayList<String> checkLink = MarkdownParse.getLinks(content);
+        ArrayList<String> checkList = new ArrayList<String>();
+
+        checkList.add("https://something.com");
+        checkList.add("some-thing.html");
+
+        assertEquals("https://something.com", checkLink.get(0));
+        assertEquals("some-thing.html", checkLink.get(1));
+        assertEquals(checkList, checkLink);
+
+        //MarkdownParse.getLinks("test-file.md");
+        //assertEquals("https://something.com", )
+
+
+    }
+
+}
+
